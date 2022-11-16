83377ff8f2519a4b6eaafaca251682c98d93f199
Eric Kanski <erickanski0402@gmail.com>
2022-11-16 14:09:16 -0600

    copypasta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ExifReader, { IccTags, Tags, ValueTag, XmpTag, XmpTags } from 'exifreader';
 
-function App() {
+export default function App() {
+  const parseDate = (s) => {
+    var b = s[0].split(/\D/);
+    var dt = new Date(b[0], b[1] - 1, b[2], b[3], b[4], b[5]);
+    return dt;
+  }
+  var clickedTime = 0;
+  const onChange = (e) => {
+    const fileNameOutput = document.getElementById("fileName");
+    const pre = document.getElementById("exif");
+    const dto = document.getElementById("dateTimeOriginal");
+    const dtoutput = document.getElementById("dateTimeOutput");
+    ExifReader
+      .load(e.target.files[0])
+      .then((tags) => {
+        console.log(tags);
+        pre.innerText = JSON.stringify(tags, null, 2);
+        if (!!tags.DateTimeOriginal) {
+          const dateTaken = tags.DateTimeOriginal.value;
+          dto.innerText = dateTaken;
+          const dateTakenAsDate = parseDate(dateTaken);
+          console.log(dateTakenAsDate);
+          if (clickedTime.getTime() < dateTakenAsDate.getTime()) {
+            dtoutput.innerText = "Photo taken live!";
+          } else {
+            dtoutput.innerText = "Photo from camera roll";
+          }
+        }
+      })
+      .catch((err) => (err.innerText = err));
+    fileNameOutput.innerText = e.target.files[0].name;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <input type="file" id="picker" onChange={onChange}/>
+      <div id="fileName"></div>
+      <div id="dateTimeOriginal"></div>
+      <div id="dateTimeOutput"></div>
+      <pre id="exif">Exif Here</pre>
     </div>
   );
 }
-
-export default App;
